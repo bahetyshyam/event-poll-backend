d62061ac947c92b5d9f56ec5674932589a76ecde
Shyam Bahety <bahetyshyam@gmail.com>
2020-05-13 20:10:23 +0530

    added event route and its corresponding file

---
+++ src/routes/event.js
@@ -0,0 +1,9 @@
+import express from "express";
+import mongoose from "mongoose";
+import models from "../models";
+import isLoggedIn from "../middleware/isLoggedIn";
+const router = express.Router();
+
+//api's go here
+
+export default router;
--- src/services/routes.js
+++ src/services/routes.js
@@ -2,6 +2,7 @@ import bodyParser from 'body-parser';
 import auth from '../routes/auth';
 import users from '../routes/user';
 import group from '../routes/group';
+import event from '../routes/event';
 
 
 export default function (app) {
@@ -11,6 +12,7 @@ export default function (app) {
     app.use('/api/auth', auth);
     app.use('/api/user', users);
     app.use('/api/group', group);
+    app.use('/api/event', event);
 
     app.all('*', (req, res) => {
         res.status(404).send({ success: false, message: 'Endpoint not found.' });
