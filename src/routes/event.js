4c440041d1b088dfa02fff029e0158216bfc1ca9
Shyam Bahety <bahetyshyam@gmail.com>
2020-05-31 15:03:31 +0530

    populated group field in event get api

--- src/routes/event.js
+++ src/routes/event.js
@@ -4,28 +4,30 @@ import mongoose from "mongoose";
 import models from "../models";
 import isLoggedIn from "../middleware/isLoggedIn";
 
-
 //Events GET API to extract events from groups that a user is in
-router.get('/', isLoggedIn, async (req, res) => {
-    const userId = req.user._id;
+router.get("/", isLoggedIn, async (req, res) => {
+  const userId = req.user._id;
 
-    try {
-        const groups = await models.group.find({ members: userId });
-        const group_ids = groups.map(g => g._id);
+  try {
+    const groups = await models.group.find({ members: userId });
+    const group_ids = groups.map((g) => g._id);
 
-        const result = await models.event.find({ group: group_ids });
+    const result = await models.event
+      .find({ group: group_ids })
+      .populate("group");
 
-        return res.status(200).send({
-            success: true,
-            events: result
-        })
-    }
-    catch (err) {
-        return res.status(401).send({
-            success: false, message: "User Not Found", error: err
-        });
-    }
-})
+    return res.status(200).send({
+      success: true,
+      events: result,
+    });
+  } catch (err) {
+    return res.status(401).send({
+      success: false,
+      message: "User Not Found",
+      error: err,
+    });
+  }
+});
 
 //Format to send the data to this api
 // {
@@ -37,31 +39,30 @@ router.get('/', isLoggedIn, async (req, res) => {
 // 	"description" : "This is a test event",
 // 	"group" : "5eb5764614fac40cd4187e57"
 // }
-router.post('/', isLoggedIn, async (req, res) => {
-    const userId = mongoose.Types.ObjectId(req.userId);
+router.post("/", isLoggedIn, async (req, res) => {
+  const userId = mongoose.Types.ObjectId(req.userId);
 
-    let schedule = new Date(req.body.schedule)
+  let schedule = new Date(req.body.schedule);
 
-    const newEvent = new models.event({
-        name: req.body.name,
-        schedule: schedule,
-        location: {
-            latitude: req.body.latitude,
-            longitude: req.body.longitude,
-            locationName: req.body.locationName
-        },
-        description: req.body.description,
-        createdBy: userId,
-        group: mongoose.Types.ObjectId(req.body.group)
-    });
+  const newEvent = new models.event({
+    name: req.body.name,
+    schedule: schedule,
+    location: {
+      latitude: req.body.latitude,
+      longitude: req.body.longitude,
+      locationName: req.body.locationName,
+    },
+    description: req.body.description,
+    createdBy: userId,
+    group: mongoose.Types.ObjectId(req.body.group),
+  });
 
-    try {
-        const savedEvent = await newEvent.save();
-        res.json({ success: true, event: savedEvent })
-    }
-    catch (err) {
-        res.status(400).json({ success: false, error: err })
-    }
-})
+  try {
+    const savedEvent = await newEvent.save();
+    res.json({ success: true, event: savedEvent });
+  } catch (err) {
+    res.status(400).json({ success: false, error: err });
+  }
+});
 
 export default router;
